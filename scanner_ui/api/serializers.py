aa4733bc15304e3a63260e302f7cbe2ec2baed48
timothy-spencer <timothy.spencer@gsa.gov>
2019-07-01 11:31:15 -0700

    changed default API response to be a reference, but leave full response code in because we will make different endpoints for it later

--- scanner_ui/api/serializers.py
+++ scanner_ui/api/serializers.py
@@ -3,5 +3,6 @@ from rest_framework import serializers
 class DomainsSerializer(serializers.Serializer):
 	domain = serializers.CharField(max_length=200)
 	scantype = serializers.CharField(max_length=200)
-	data = serializers.DictField()
+	data = serializers.DictField(required=False)
+	url = serializers.CharField(max_length=400)
 	lastmodified = serializers.DateTimeField()
--- scanner_ui/api/views.py
+++ scanner_ui/api/views.py
@@ -16,8 +16,14 @@ def getScanFromS3(path):
 	response = s3_resource.Object(settings.BUCKETNAME,path).get()
 	return {'body': response['Body'].read(), 'lastmodified': response['LastModified']}
 
+def getScanInfoFromS3(path):
+	s3_resource = boto3.resource('s3')
+	response = s3_resource.Object(settings.BUCKETNAME,path).get()
+	return {'body': response['Body'], 'lastmodified': response['LastModified']}
+
+# This function pulls down the scan data and puts it inline.
 # If scantype is None, then we want all the scans.
-def getScantype(scantype=None):
+def getFullScantype(scantype=None):
 	scans = []
 	for f in getMetadatafromS3():
 		if not re.search('\.json$', f.key):
@@ -27,12 +33,30 @@ def getScantype(scantype=None):
 			scandomain = os.path.basename(os.path.splitext(f.key)[0])
 			scandata = getScanFromS3(f.key)
 			json_data = json.loads(scandata['body'])
+			scan_data_url = "https://s3-" + os.environ['AWS_DEFAULT_REGION'] + ".amazonaws.com/" + settings.BUCKETNAME + "/" + f.key
 			lastmodified = scandata['lastmodified']
-			scans.append({"domain": scandomain, "scantype": s3scantype, "path": f.key, "lastmodified": lastmodified, "data": json_data})
+			scans.append({"domain": scandomain, "scantype": s3scantype, "path": f.key, "lastmodified": lastmodified, "data": json_data, "url": scan_data_url})
 	return scans
 
+# This function provides a reference to the domain scan data in it's s3 bucket
+# If scantype is None, then we want all the scans.
+def getScantype(scantype=None):
+	scans = []
+	for f in getMetadatafromS3():
+		if not re.search('\.json$', f.key):
+			continue
+		s3scantype = os.path.dirname(f.key)
+		if scantype == None or s3scantype == scantype:
+			scandomain = os.path.basename(os.path.splitext(f.key)[0])
+			scandata = getScanInfoFromS3(f.key)
+			scan_data_url = "https://s3-" + os.environ['AWS_DEFAULT_REGION'] + ".amazonaws.com/" + settings.BUCKETNAME + "/" + f.key
+			lastmodified = scandata['lastmodified']
+			scans.append({"domain": scandomain, "scantype": s3scantype, "path": f.key, "lastmodified": lastmodified, "url": scan_data_url})
+	return scans
+
+# This function pulls down the domain scan data and puts it inline.
 # If domain is None, then we want all the domains.
-def getDomain(domain=None):
+def getFullDomain(domain=None):
 	scans = []
 	for f in getMetadatafromS3():
 		scandomain = os.path.basename(os.path.splitext(f.key)[0])
@@ -40,8 +64,23 @@ def getDomain(domain=None):
 			scantype = os.path.dirname(f.key)
 			scandata = getScanFromS3(f.key)
 			json_data = json.loads(scandata['body'])
+			scan_data_url = "https://s3-" + os.environ['AWS_DEFAULT_REGION'] + ".amazonaws.com/" + settings.BUCKETNAME + "/" + f.key
 			lastmodified = scandata['lastmodified']
 			scans.append({"domain": scandomain, "scantype": scantype, "path": f.key, "lastmodified": lastmodified, "data": json_data})
+	return scans
+
+# This function provides a reference to the scan data in it's s3 bucket
+# If domain is None, then we want all the domains.
+def getDomain(domain=None):
+	scans = []
+	for f in getMetadatafromS3():
+		scandomain = os.path.basename(os.path.splitext(f.key)[0])
+		if domain == None or scandomain == domain:
+			scantype = os.path.dirname(f.key)
+			scandata = getScanInfoFromS3(f.key)
+			scan_data_url = "https://s3-" + os.environ['AWS_DEFAULT_REGION'] + ".amazonaws.com/" + settings.BUCKETNAME + "/" + f.key
+			lastmodified = scandata['lastmodified']
+			scans.append({"domain": scandomain, "scantype": scantype, "path": f.key, "lastmodified": lastmodified, "url": scan_data_url})
 	return scans
 
 def getMetadatafromS3():
