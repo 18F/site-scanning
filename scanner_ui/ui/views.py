a795a9a2dc940aedb82c6b0569a081f373ca6287
timothy-spencer <timothy.spencer@gsa.gov>
2019-07-16 16:33:43 -0700

    fix up queries to be real

--- scanner_ui/ui/views.py
+++ scanner_ui/ui/views.py
@@ -10,11 +10,20 @@ es = Elasticsearch([os.environ['ESURI']])
 
 def index(request):
 	yesterdayindex = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d-") + '*'
+
 	allscanscount = Search(using=es, index=yesterdayindex).query().count()
 
+	scantypecount = len(es.indices.get_alias(yesterdayindex))
+
+	s = Search(using=es, index=yesterdayindex).query().source(['domain'])
+	domainmap = {}
+	for i in s.scan():
+	        domainmap[i.domain] = 1
+	domaincount = len(domainmap)
+
 	context = {
-		'num_domains': 100,
-		'num_scantypes': 2,
+		'num_domains': domaincount,
+		'num_scantypes': scantypecount,
 		'num_scans': allscanscount,
 	}
 	return render(request, "index.html", context=context)
