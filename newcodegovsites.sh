374180e19b59945f82919095d846b84ff8942405
timothy-spencer <timothy.spencer@gsa.gov>
2019-10-08 14:27:47 -0700

    made this work on linux for real, check for required tools, allow them to specify how far back to compare

--- newcodegovsites.sh
+++ newcodegovsites.sh
@@ -1,12 +1,42 @@
 #!/bin/sh
 #
-# This script will look at the differences between the last 5 days of present
-# code.json files and emit a list of new/removed domains
+# This script will look at the differences between the last N days of present
+# code.json files and emit a list of new/removed domains.
+#
+# If you supply an argument, it will try to compare N days back.  If you do not
+# supply an argument, it will compare 7 days back.
+#
+# usage examples:
+#    ./newcodegovsites.sh
+#    ./newcodegovsites.sh 5
 #
 
-YESTERDAY="$(date -v-5d +%F)"
+if command -v jq >/dev/null ; then
+	echo "jq is here" >/dev/null
+else
+	echo "jq is required for this script to work.  Please install it (https://stedolan.github.io/jq/download/) and come back!"
+	exit 1
+fi
+
+if command -v curl >/dev/null ; then
+	echo "curl is here" >/dev/null
+else
+	echo "curl is required for this script to work.  Please install it and come back!"
+	exit 2
+fi
+
+if [ -z "$1" ] ; then
+	DAYSAGO=7
+else
+	DAYSAGO="$1"
+fi
+
+EARLIER="$(date -v-"$DAYSAGO"d +%F)"
 if [ "$?" -gt 0 ] ; then
-	YESTERDAY="$(date -d "yesterday 13:00" '+%Y-%m-%d')"
+	NOW=$(date +%s)
+	let SECONDS=86400*$DAYSAGO
+	let EARLIER=$NOW-$SECONDS
+	EARLIER="$(date -d "$EARLIER" -D '%s' '+%Y-%m-%d')"
 fi
 
 TODAY="$(date '+%F')"
@@ -15,9 +45,9 @@ if [ "$?" -gt 0 ] ; then
 fi
 
 curl -s "https://site-scanning.app.cloud.gov/search200/json/?200page=/code.json&date=${TODAY}&mimetype=application/json&present=Present" | jq -r '.[] | .domain' | sort > /tmp/todaydomains.$$
-curl -s "https://site-scanning.app.cloud.gov/search200/json/?200page=/code.json&date=${YESTERDAY}&mimetype=application/json&present=Present" | jq -r '.[] | .domain' | sort > /tmp/yesterdaydomains.$$
+curl -s "https://site-scanning.app.cloud.gov/search200/json/?200page=/code.json&date=${EARLIER}&mimetype=application/json&present=Present" | jq -r '.[] | .domain' | sort > /tmp/earlierdomains.$$
 
-echo "# Comparing $YESTERDAY with $TODAY:"
-diff /tmp/yesterdaydomains.$$ /tmp/todaydomains.$$
+echo "# Comparing $EARLIER with $TODAY:"
+diff /tmp/earlierdomains.$$ /tmp/todaydomains.$$
 
-rm -f /tmp/yesterdaydomains.$$ /tmp/todaydomains.$$
+rm -f /tmp/earlierdomains.$$ /tmp/todaydomains.$$
