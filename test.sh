274e62a64b32f72aa0a88859a4c8e6de5d990302
Tim Spencer <timothy.spencer@gsa.gov>
2019-11-18 12:36:25 -0800

    Made third party services UI nicer looking, fixing DAP True/Falseness, added and commented out OWASP ZAP scanner (#223)

--- test.sh
+++ test.sh
@@ -27,6 +27,23 @@ done
 docker exec "$CONTAINER" ./composetest.sh
 SAVEDEXIT=$?
 
+# # do an OWASP ZAP scan XXX this takes too long
+# docker exec "$CONTAINER" ./manage.py migrate
+# export ZAP_CONFIG=" \
+#   -config globalexcludeurl.url_list.url\(0\).regex='.*/robots\.txt.*' \
+#   -config globalexcludeurl.url_list.url\(0\).description='Exclude robots.txt' \
+#   -config globalexcludeurl.url_list.url\(0\).enabled=true \
+#   -config spider.postform=true"
+# CONTAINER=$(docker-compose images | awk '/zaproxy/ {print $1}')
+# echo "====================================== OWASP ZAP tests"
+# docker exec "$CONTAINER" zap-full-scan.py -t http://scanner-ui:8000/about/ -m 5 -z "${ZAP_CONFIG}" | tee /tmp/zap.out 
+# if grep 'FAIL-NEW: 0' /tmp/zap.out >/dev/null ; then
+# 	echo 'passed OWASP ZAP'
+# else
+# 	ecgi 'OWASP ZAP scan failed'
+# 	SAVEDEXIT=1
+# fi
+
 # clean up (if desired)
 if [ "$1" != "nodelete" ] ; then
 	docker-compose down
