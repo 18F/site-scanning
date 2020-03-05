174ab7f07d0f2f376ac902666f17300995686ffa
timothy-spencer <timothy.spencer@gsa.gov>
2020-03-04 17:00:14 -0800

    added code to allow us to append csv files onto the existing domains.csv file without duplicating domains or losing metadata

--- getdomains.sh
+++ getdomains.sh
@@ -5,23 +5,29 @@
 #
 
 # This is how many domains to scan in a single task
-BATCHSIZE="${BATCHSIZE:-2000}"
+BATCHSIZE="${BATCHSIZE:-3000}"
 
 # get the list of domains
 if [ -f "$DOMAINCSV" ] ; then
 	# this is so we can supply our own file for testing
 	cp "$DOMAINCSV" /tmp/domains.csv
 else
+	# This is the base domain file with metadata in it already
 	wget -O /tmp/domains.csv https://github.com/GSA/data/raw/master/dotgov-domains/current-federal.csv
 
-	## If you add more domains (like subdomains), append them on to
-	## /tmp/domains.csv in the https://github.com/GSA/data/raw/master/dotgov-domains/current-federal.csv
-	## format, so that when we load them in, we will be able to parse out
-	## the agency, branch, etc.
-	curl -s https://raw.githubusercontent.com/GSA/data/master/dotgov-websites/other-websites.csv | tail -n +2 | awk -F, '{printf("%s,,,,,,\n",$1)}' >> /tmp/domains.csv
+	# XXX HERE IS WHERE TO ADD MORE DOMAINS/SUBDOMAINS!!!
+	# To add more files, make sure they are in the same format as the
+	# current-federal.csv file, OR make sure that the domain is the first
+	# field in your csv file.  The mergedomaincsv.py script will make sure that
+	# there are no duplicate domains, and that the metadata from the first
+	# instance of a domain remains.
+	# Also: Make sure that the final file created ends up being called /tmp/domains.csv!
+
+	wget -O /tmp/other-websites.csv https://raw.githubusercontent.com/GSA/data/master/dotgov-websites/other-websites.csv
+	./mergedomaincsv.py /tmp/domains.csv /tmp/other-websites.csv /tmp/mergeddomains.csv
+	mv /tmp/mergeddomains.csv /tmp/domains.csv
 fi
 
-# XXX uniq the list here, merge metadata if we can
 
 # figure out where to split the CSV up
 if [ -n "$1" ] ; then
