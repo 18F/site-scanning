84d200cd25384985e07e695d433106c02e35699e
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-08-19 18:18:11 -0400

    Remove end to end tests for now.
    Why: Right now we have a suite of end-to-end tests. These use live page data
    [18f.gov, login.gov, afrh.gov, etc] from the Scan Engine. So, while these
    tests are a simple way to test the entire system of dependencies, they are
    dependent on the data from live pages to operate. This means if the data on
    those pages change or if the sites are not available, we get breakages in
    our build even if nothing changed on our end. We are also removing the
    puppeteer dependencies from the Dockerfile to account for the flakiness with that.
    
    Note: this is a temporary state, and we will add the tests back in with
    a new strategy.
    
    Additionally, adding a new endpoint "meta" and one API test for that.
    
    How: Removing the flaky end-to-end tests, adding the meta API endpoint,
    updating the Dockerfile.
    
    Tags: testing, Docker, puppeteer.

--- composetest.sh
+++ composetest.sh
@@ -20,45 +20,49 @@ aws "$S3ENDPOINT" s3 mb "s3://$BUCKETNAME"
 ./manage.py migrate || cleanup "could not do db migrations"
 
 # load data into our environment
-export BATCHSIZE=4
-export DOMAINCSV="/tmp/testdomains.csv"
-cat <<EOF > "$DOMAINCSV"
-Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
-18F.GOV,Federal Agency - Executive,General Services Administration,18F,Washington,DC,tts-vulnerability-reports@gsa.gov
-GSA.GOV,Federal Agency - Executive,General Services Administration,GSA,Washington,DC,(blank)
-AFRH.GOV,Federal Agency - Executive,Armed Forces Retirement Home,Armed Forces Retirement Home,Washington,DC,(blank)
-CLOUD.GOV,Federal Agency - Executive,General Services Administration,18F | GSA,Washington,DC,tts-vulnerability-reports@gsa.gov
-LOGIN.GOV,Federal Agency - Executive,General Services Administration,General Services Administration,Washington,DC,tts-vulnerability-reports@gsa.gov
-calendar.gsa.gov,,,,,,
-*.ecmapps.treasuryecm.gov,,,,,,
-EOF
-./scan_engine.sh
+
+# Disabling this for now.
+
+# export BATCHSIZE=4
+# export DOMAINCSV="/tmp/testdomains.csv"
+# cat <<EOF > "$DOMAINCSV"
+# Domain Name,Domain Type,Agency,Organization,City,State,Security Contact Email
+# 18F.GOV,Federal Agency - Executive,General Services Administration,18F,Washington,DC,tts-vulnerability-reports@gsa.gov
+# GSA.GOV,Federal Agency - Executive,General Services Administration,GSA,Washington,DC,(blank)
+# AFRH.GOV,Federal Agency - Executive,Armed Forces Retirement Home,Armed Forces Retirement Home,Washington,DC,(blank)
+# CLOUD.GOV,Federal Agency - Executive,General Services Administration,18F | GSA,Washington,DC,tts-vulnerability-reports@gsa.gov
+# LOGIN.GOV,Federal Agency - Executive,General Services Administration,General Services Administration,Washington,DC,tts-vulnerability-reports@gsa.gov
+# calendar.gsa.gov,,,,,,
+# *.ecmapps.treasuryecm.gov,,,,,,
+# EOF
+# ./scan_engine.sh
 
 # run app test suite
 ./manage.py test || cleanup "python test suite exited uncleanly"
 
-SCANTYPES="
-	pagedata
-	200scanner
-	uswds2
-	sitemap
-	privacy
-	dap
-	third_parties
-	lighthouse
-"
+# Disabling this for now
+# SCANTYPES="
+# 	pagedata
+# 	200scanner
+# 	uswds2
+# 	sitemap
+# 	privacy
+# 	dap
+# 	third_parties
+# 	lighthouse
+# "
 
-# do a test that checks if the s3 bucket contains data
-echo "checking what is in the s3 bucket"
-for i in $SCANTYPES ; do
-	aws "$S3ENDPOINT" s3 ls "s3://$BUCKETNAME/$i/" | grep 18f.gov >/dev/null || cleanup "$i s3 bucket does not contain the 18f.gov scan"
-done
+# # do a test that checks if the s3 bucket contains data
+# echo "checking what is in the s3 bucket"
+# for i in $SCANTYPES ; do
+# 	aws "$S3ENDPOINT" s3 ls "s3://$BUCKETNAME/$i/" | grep 18f.gov >/dev/null || cleanup "$i s3 bucket does not contain the 18f.gov scan"
+# done
 
-# test that there are indexes available
-echo "checking whether indexes were created"
-for i in $SCANTYPES ; do
-	curl -s "$ESURL"/_cat/indices?v | grep $i >/dev/null || cleanup "the $i index was not created"
-done
+# # test that there are indexes available
+# echo "checking whether indexes were created"
+# for i in $SCANTYPES ; do
+# 	curl -s "$ESURL"/_cat/indices?v | grep $i >/dev/null || cleanup "the $i index was not created"
+# done
 
 # everything must be great!
 cleanup
--- scanner_ui/api/urls.py
+++ scanner_ui/api/urls.py
@@ -1,75 +1,62 @@
 from django.urls import path
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
-
-from .views import DomainsViewset
-from .views import ScansViewset
-from .views import ListsViewset
-from . import views
-
-
-domains_list = DomainsViewset.as_view({
-    'get': 'list'
-})
-domains_detail = DomainsViewset.as_view({
-    'get': 'retrieve'
-})
-scans_list = ScansViewset.as_view({
-    'get': 'list'
-})
-scans_detail = ScansViewset.as_view({
-    'get': 'retrieve'
-})
-scan = ScansViewset.as_view({
-    'get': 'scan'
-})
-agencies = ListsViewset.as_view({
-    'get': 'agencies'
-})
-domaintypes = ListsViewset.as_view({
-    'get': 'domaintypes'
-})
-fieldvalues = ListsViewset.as_view({
-    'get': 'fieldvalues'
-})
-dates = ListsViewset.as_view({
-    'get': 'dates'
-})
-
 from rest_framework_swagger.views import get_swagger_view
 
-swagger_view = get_swagger_view(
-    title='Spotlight API'
-)
+from . import views
+from .views import DomainsViewset, ListsViewset, ScansViewset, meta
+
+domains_list = DomainsViewset.as_view({"get": "list"})
+domains_detail = DomainsViewset.as_view({"get": "retrieve"})
+scans_list = ScansViewset.as_view({"get": "list"})
+scans_detail = ScansViewset.as_view({"get": "retrieve"})
+scan = ScansViewset.as_view({"get": "scan"})
+agencies = ListsViewset.as_view({"get": "agencies"})
+domaintypes = ListsViewset.as_view({"get": "domaintypes"})
+fieldvalues = ListsViewset.as_view({"get": "fieldvalues"})
+dates = ListsViewset.as_view({"get": "dates"})
+swagger_view = get_swagger_view(title="Spotlight API")
 
 urlpatterns = [
-    path('', swagger_view, name="docs"),
-    path('domains/', domains_list, name="domains-list"),
-    path('domains/<domain>/', domains_detail, name="domains-detail"),
-    path('domains/<domain>/<scantype>/', scan, name="scan"),
-
-    path('scans/', scans_list, name="scans-list"),
-    path('scans/<scantype>/', scans_detail, name="scans-detail"),
-    path('scans/<scantype>/csv/', views.retrieve_csv, name="scans-csv"),
-    path('scans/<scantype>/<domain>/', scan, name="scan"),
-
-    path('lists/<scantype>/agencies/', agencies, name="agencies"),
-    path('lists/<scantype>/domaintypes/', domaintypes, name="domaintypes"),
-    path('lists/<scantype>/values/<field>/', fieldvalues, name="fieldvalues"),
-    path('lists/<scantype>/values/<field>/<subfield>/', fieldvalues, name="subfieldvalues"),
-    path('lists/dates/', dates, name="dates"),
-
-    path('date/<date>/domains/', domains_list, name="date-domains-list"),
-    path('date/<date>/domains/<domain>/', domains_detail, name="date-domains-detail"),
-    path('date/<date>/domains/<domain>/<scantype>/', scan, name="date-scan"),
-
-    path('date/<date>/scans/', scans_list, name="date-scans-list"),
-    path('date/<date>/scans/<scantype>/', scans_detail, name="date-scans-detail"),
-    path('date/<date>/scans/<scantype>/csv/', views.retrieve_csv, name="date-scans-csv"),
-    path('date/<date>/scans/<scantype>/<domain>/', scan, name="date-scan"),
-
-    path('date/<date>/lists/<scantype>/agencies/', agencies, name="date-agencies"),
-    path('date/<date>/lists/<scantype>/domaintypes/', domaintypes, name="date-domaintypes"),
-    path('date/<date>/lists/<scantype>/values/<field>/', fieldvalues, name="date-fieldvalues"),
-    path('date/<date>/lists/<scantype>/values/<field>/<subfield>/', fieldvalues, name="date-subfieldvalues"),
+    path("", swagger_view, name="docs"),
+    path("meta/", meta, name="meta"),
+    path("domains/", domains_list, name="domains-list"),
+    path("domains/<domain>/", domains_detail, name="domains-detail"),
+    path("domains/<domain>/<scantype>/", scan, name="scan"),
+    path("scans/", scans_list, name="scans-list"),
+    path("scans/<scantype>/", scans_detail, name="scans-detail"),
+    path("scans/<scantype>/csv/", views.retrieve_csv, name="scans-csv"),
+    path("scans/<scantype>/<domain>/", scan, name="scan"),
+    path("lists/<scantype>/agencies/", agencies, name="agencies"),
+    path("lists/<scantype>/domaintypes/", domaintypes, name="domaintypes"),
+    path("lists/<scantype>/values/<field>/", fieldvalues, name="fieldvalues"),
+    path(
+        "lists/<scantype>/values/<field>/<subfield>/",
+        fieldvalues,
+        name="subfieldvalues",
+    ),
+    path("lists/dates/", dates, name="dates"),
+    path("date/<date>/domains/", domains_list, name="date-domains-list"),
+    path("date/<date>/domains/<domain>/", domains_detail, name="date-domains-detail"),
+    path("date/<date>/domains/<domain>/<scantype>/", scan, name="date-scan"),
+    path("date/<date>/scans/", scans_list, name="date-scans-list"),
+    path("date/<date>/scans/<scantype>/", scans_detail, name="date-scans-detail"),
+    path(
+        "date/<date>/scans/<scantype>/csv/", views.retrieve_csv, name="date-scans-csv"
+    ),
+    path("date/<date>/scans/<scantype>/<domain>/", scan, name="date-scan"),
+    path("date/<date>/lists/<scantype>/agencies/", agencies, name="date-agencies"),
+    path(
+        "date/<date>/lists/<scantype>/domaintypes/",
+        domaintypes,
+        name="date-domaintypes",
+    ),
+    path(
+        "date/<date>/lists/<scantype>/values/<field>/",
+        fieldvalues,
+        name="date-fieldvalues",
+    ),
+    path(
+        "date/<date>/lists/<scantype>/values/<field>/<subfield>/",
+        fieldvalues,
+        name="date-subfieldvalues",
+    ),
 ]
