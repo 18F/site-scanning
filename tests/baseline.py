2f7e703a1057519fb9df660fc2477de8e53147a7
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-08-27 19:00:17 -0400

    Add some logging to the api baselines. Why: Get a clearer idea of what is and isn't working in general.
    How: Add print statements to the script.
    
    Tags: api, testing

--- tests/baseline.py
+++ tests/baseline.py
@@ -12,6 +12,7 @@ import asyncio
 import csv
 import dataclasses
 import datetime
+from collections import Counter
 from itertools import islice, product
 from random import SystemRandom
 from typing import Any, List, Union, Tuple
@@ -21,7 +22,7 @@ import httpx
 
 API_URL = "https://site-scanning.app.cloud.gov/"
 API_VERSION = "v1"
-NUM_TESTS = 100
+NUM_DOMAINS = 200
 SCAN_TYPES = [
     "pagedata",
     "200scanner",
@@ -110,14 +111,37 @@ def random_list(num: int, list_: List[Any]) -> List[Any]:
     return list_[:num]
 
 
-async def main():
-    domains = await get_domains()
-    random_domains = random_list(NUM_TESTS, domains)
-    requests = construct_requests(random_domains, SCAN_TYPES, date="2020-08-26")
-    results = await test_api(requests)
+def process_responses(responses: Tuple[Response]):
+    error_count = 0
+    errors = []
+    total_requests = NUM_DOMAINS * len(SCAN_TYPES)
 
-    for result in results:
-        print(f"Request to {result.request.url} returned a {result.status}")
+    for response in responses:
+        if response.status != 200:
+            error_count += 1
+            errors.append(response)
+
+    success_rate = ((total_requests - error_count) / total_requests) * 100
+    print(f"{total_requests} requests with {error_count} error(s)")
+    print(f"Success rate: {success_rate}")
+
+    if error_count:
+        print("\nDisplaying errors...")
+        counter = Counter()
+        for error in errors:
+            counter[error.request.scantype] += 1
+
+        for k, v in counter.items():
+            print(f"Found {v} errors for the {k} scantype.")
+
+
+async def main():
+    print("Calculating baselines...")
+    domains = await get_domains()
+    random_domains = random_list(NUM_DOMAINS, domains)
+    requests = construct_requests(random_domains, SCAN_TYPES, date="2020-08-26")
+    responses = await test_api(requests)
+    process_responses(responses)
 
     await client.aclose()
     return random_domains
